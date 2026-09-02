369a8122cf7756b4c4f828a81e288e3950832c06
PippiDK <133820767+PippiDK@users.noreply.github.com>
2026-09-02 22:45:28 +0200

    feat(test): register SuccessFactors company sites

--- app/lib/company-watch.js
+++ app/lib/company-watch.js
@@ -3,6 +3,9 @@ export const COMPANY_WATCH_STORAGE_KEY='applypilot-company-watch-v1'
 export const COMPANY_CONNECTORS={
   '3Shape':{status:'connected',connector:'teamtailor',baseUrl:'https://careers.3shape.com'},
   'NoA Ignite Denmark':{status:'connected',connector:'teamtailor',baseUrl:'https://careers-dk.noaignite.com'},
+  'Coloplast':{status:'connected',connector:'successfactors',baseUrl:'https://careers.coloplast.com',listingPath:'/go/BusSupport_it/4775001/'},
+  'Vestas':{status:'connected',connector:'successfactors',baseUrl:'https://careers.vestas.com',listingPath:'/go/All-Jobs/3298601/'},
+  'DSV':{status:'connected',connector:'successfactors',baseUrl:'https://jobs.dsv.com',listingPath:'/go/Jobs%40DSV/2713001/'},
 }
 
 export const TARGET_COMPANIES=[
